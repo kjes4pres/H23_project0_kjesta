f80add2ba009fde6265a027727224963ab85a25a
kjes4pres <stangelandkjersti@gmail.com>
2023-09-05 09:17:23 +0200

    trying to make a parametrized test for division

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import pytest 
 
-from calculator import add
+from calculator import *
 
 @pytest.mark.parametrize(
         "x, y, expected", [(1, 2, 3), (2, 2, 4), (1, 1, 2)]
@@ -10,3 +10,12 @@ def test_add(x, y, expected):
     success = computed == expected
     message = f"Computed value was {computed}, expected value was {expected}."
     assert success, message
+
+@pytest.mark.parametrize(
+        "x, y, expected", [(2,2,1),(1, 0.5, 2),(0.1, 100, 0.001)]
+)
+def test_divide(x, y, expected):
+    computed = divide(x, y)
+    success = computed == expected or m.isclose(expected, computed) == True
+    message = f"Computed value was {computed}, expected value was {expected}."
+    assert success, message 
