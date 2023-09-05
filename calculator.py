2a323cb93f04fdc99a2e6f9a7ea03d2581910b4e
kjes4pres <stangelandkjersti@gmail.com>
2023-09-05 09:39:17 +0200

    Updated doc-strings in functions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 def add(x: float, y: float) -> float:
+    '''Addition of two floats.'''
     return x + y
 
 def divide(x: float, y: float) -> float:
+    '''Divison of two floats'''
     return x/y
