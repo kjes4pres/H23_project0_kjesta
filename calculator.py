f4982be90edb77605ab54b715ad79a022c6e911c
kjes4pres <stangelandkjersti@gmail.com>
2023-09-05 11:00:35 +0200

    fixed code stile, whitespaces etc.

--- calculator.py
+++ calculator.py
@@ -2,12 +2,13 @@ def add(x: float, y: float) -> float:
     '''Addition of two floats.'''
     return x + y
 
+
 def divide(x: float, y: float) -> float:
     '''Divison of two floats'''
     return x/y
 
 
-def factorial(x:float) -> float:
+def factorial(x: float) -> float:
     '''Take factorial of a number, ex.: 5! = 1*2*3*4*5 = 120'''
     counter = 1
     factorial_result = 1
@@ -17,11 +18,15 @@ def factorial(x:float) -> float:
     return factorial_result
 
 
-def sin(x:float) -> float:
+def sin(x: float) -> float:
     '''Calculate sin(x) using  the truncated Taylor series expansion'''
     N = 20
     result = 0
     for n in range(N + 1):
         result += (((-1)**n)*x**(2*n + 1))/factorial(2*n + 1)
     return result
+
+
+def mean(x: list) -> float:
+    '''Calculate the mean value of a list of numbers'''
 
