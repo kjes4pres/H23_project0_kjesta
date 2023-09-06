6d4d2560151c22ece6236ad2f3d0e297beed4c78
kjes4pres <stangelandkjersti@gmail.com>
2023-09-06 11:49:12 +0200

    Reformatted file using black.

--- test_calculator.py
+++ test_calculator.py
@@ -1,31 +1,27 @@
-import pytest 
+import pytest
 import math as m
 import numpy as np
 
 from calculator import *
 
-@pytest.mark.parametrize(
-        "x, y, expected", [(1, 2, 3), (2, 2, 4), (1, 1, 2)]
-)
+
+@pytest.mark.parametrize("x, y, expected", [(1, 2, 3), (2, 2, 4), (1, 1, 2)])
 def test_add(x, y, expected):
     computed = add(x, y)
     success = computed == expected or m.isclose(expected, computed) == True
     message = f"Computed value was {computed}, expected value was {expected}."
     assert success, message
 
-@pytest.mark.parametrize(
-        "x, y, expected", [(2,2,1),(1, 0.5, 2),(0.1, 100, 0.001)]
-)
+
+@pytest.mark.parametrize("x, y, expected", [(2, 2, 1), (1, 0.5, 2), (0.1, 100, 0.001)])
 def test_divide(x, y, expected):
     computed = divide(x, y)
     success = computed == expected or m.isclose(expected, computed) == True
     message = f"Computed value was {computed}, expected value was {expected}."
-    assert success, message 
+    assert success, message
 
 
-@pytest.mark.parametrize(
-    "x, expected", [(0,1), (2,2), (5,120)]
-)
+@pytest.mark.parametrize("x, expected", [(0, 1), (2, 2), (5, 120)])
 def test_factorial(x, expected):
     computed = factorial(x)
     success = computed == expected or m.isclose(expected, computed) == True
@@ -34,7 +30,8 @@ def test_factorial(x, expected):
 
 
 @pytest.mark.parametrize(
-        "x, expected", [(0, 0), (np.pi/4, 1/(np.sqrt(2))), (np.pi/2, 1), (3*np.pi/2, -1)]
+    "x, expected",
+    [(0, 0), (np.pi / 4, 1 / (np.sqrt(2))), (np.pi / 2, 1), (3 * np.pi / 2, -1)],
 )
 def test_sin(x, expected):
     computed = sin(x)
@@ -54,7 +51,7 @@ def test_mean(list, expected):
 
 
 @pytest.mark.parametrize(
-    "list, expected", [([1,2,3], 2/3), ([1,1,1,1], 0), ([5,10], 6.25)]
+    "list, expected", [([1, 2, 3], 2 / 3), ([1, 1, 1, 1], 0), ([5, 10], 6.25)]
 )
 def test_var(list, expected):
     computed = var(list)
@@ -62,14 +59,17 @@ def test_var(list, expected):
     message = f"Computed value was {computed}, expected value was {expected}."
     assert success, message
 
+
 def test_factorial_raises_ValueError_for_negatives():
     with pytest.raises(ValueError):
         factorial(-1)
 
+
 def test_factorial_raises_TypeError_for_noninteger():
     with pytest.raises(TypeError):
-        factorial(1/3)
+        factorial(1 / 3)
+
 
 def test_divide_raises_ZeroDivisionError():
     with pytest.raises(ZeroDivisionError):
-        divide(5,0)
+        divide(5, 0)
