953a247be4d7d458281d7c2152057acb09748d24
kjes4pres <stangelandkjersti@gmail.com>
2023-09-07 17:14:28 +0200

    Reformatted with black

--- numerical_integration.py
+++ numerical_integration.py
@@ -1,36 +1,37 @@
 from typing import Callable
 
+
 def left_riemann_sum(f: Callable[[float], float], a: float, b: float, n: int) -> float:
-    h = (b - a)/n
+    h = (b - a) / n
     riemann_sum = 0
     for i in range(n):
-        x_i = a + i*h
-        riemann_sum += h*f(x_i)
+        x_i = a + i * h
+        riemann_sum += h * f(x_i)
     return riemann_sum
 
 
 def midpoint(f: Callable[[float], float], a: float, b: float, n: int) -> float:
-
     if not isinstance(n, int) or n <= 0:
         raise ValueError(f"n must be an integer and bigger than zero, not {n}.")
     if b <= a:
-        raise ValueError("The upper limit (b) must be greater than the lower limit (a).")
+        raise ValueError(
+            "The upper limit (b) must be greater than the lower limit (a)."
+        )
 
-    h = (b - a)/n
+    h = (b - a) / n
     midpoint_result = 0
     for i in range(n):
-        x_i = a + i*h
-        midpoint_result += (h/2)*(f(x_i) + f(x_i + h))
+        x_i = a + i * h
+        midpoint_result += (h / 2) * (f(x_i) + f(x_i + h))
     return midpoint_result
 
 
 def integrate(
     f: Callable[[float], float], a: float, b: float, n: int, method: str = "midpoint"
 ) -> float:
-    
     if method not in ["midpoint", "left_riemann_sum"]:
         raise ValueError("Only methods allowed is midpoint or left_riemann_sum.")
-        
+
     if method == "midpoint":
         return midpoint(f, a, b, n)
     else:
--- test_numerical_integration.py
+++ test_numerical_integration.py
@@ -3,13 +3,15 @@ import numpy as np
 from numerical_integration import *
 import pytest
 
+
 def f(x):
-    return 3*(x**2)*np.exp(x**3)
+    return 3 * (x**2) * np.exp(x**3)
+
 
 a = 0
 b = 1
 
-expected = (np.exp(b**3) - np.exp(a**3))
+expected = np.exp(b**3) - np.exp(a**3)
 
 
 def test_left_riemann_sum():
@@ -26,14 +28,13 @@ def test_midpoint():
     n = 70
     computed = midpoint(f, a, b, n)
     diff = abs(computed - expected)
-    success =  diff < 0.001
+    success = diff < 0.001
     message = f"Computed integral differs from analytical integral with {diff}"
     assert success, message
-    
+
 
 @pytest.mark.parametrize("method, n", [("midpoint", 1000), ("left_riemann_sum", 5000)])
 def test_integrate(method, n):
-
     computed = integrate(f, a, b, n, method)
     diff = abs(computed - expected)
     success = diff < 0.001
@@ -46,4 +47,3 @@ def test_integrate_raises_ValueError_for_invalid_method():
     with pytest.raises(ValueError):
         n = 5000
         integrate(f, a, b, n, "notmidpoint")
-
